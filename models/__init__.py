7e229f6490c1ed39a6b8f1755240bb12c33daff8
excelsiorcjh <excelsiorcjh@gmail.com>
2020-05-22 17:12:41 +0900

    rouge score

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,17 @@
 from .basic_summarizer import *
 from .naive_summarizer import *
 from .summarizer import *
+from .lstm import LSTM
+from .gat import GATClassifier
 
 
 summarize_models = {
     'NaiveSummarizer': NaiveSummarizer,
     'BasicSummarizer': BasicSummarizer,
     'Summarizer': Summarizer,
+}
+
+base_models = {
+    'LSTM': LSTM,
+    'GATClassifier': GATClassifier,
 }
